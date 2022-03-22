36fc67556a6f15c0aa2b19c95d381611c2bddd40
Michel San <michel1.san@epita.fr>
2022-03-22 12:15:57 +0100

    freezer: fix merge

--- freezer/freezer_netlink.c
+++ freezer/freezer_netlink.c
@@ -9,61 +9,20 @@
 #define NETLINK_USER 31
 #define NETLINK_LOG  "[freezer][netlink] "
 
-struct sock *nl_sk = NULL;
+struct sock* nl_sk = NULL;
 
-static void freezer_recv_msg(struct sk_buff *skb)
+static void freezer_recv_msg(struct sk_buff* skb)
 {
-    struct nlmsghdr *nlh;
-    struct sk_buff *skb_out;
+    struct nlmsghdr* nlh;
+    struct sk_buff* skb_out;
     int pid = 0;
     int res = 0;
     int freezer_wrapper_res = 0;
 
     printk(KERN_INFO NETLINK_LOG "entering: %s\n", __FUNCTION__);
 
-    nlh = (struct nlmsghdr *)skb->data;
-    struct netlink_cmd *data = (struct netlink_cmd *) nlmsg_data(nlh);
-    printk(KERN_INFO NETLINK_LOG "resource: %d\n", data->resource);
-    printk(KERN_INFO NETLINK_LOG "uid: %d\n", data->uid);
-    pid = nlh->nlmsg_pid; /*pid of sending process */
-
-    // process the payload
-    freezer_wrapper_res = freezer_call_wrapper(data) == 0 ? NLMSG_DONE : NLMSG_ERROR;
-
-    // send response back
-    skb_out = nlmsg_new(sizeof(int), 0);
-    if (!skb_out)
-    {
-        printk(KERN_ERR NETLINK_LOG "failed to allocate new skb\n");
-        return;
-    }
-
-    // skb, portid, seq, type, payload, flags
-    // put the empty message (with response in the msg.type) on socket buffer
-    nlh = nlmsg_put(skb_out, 0, 0, freezer_wrapper_res, 0, 0);
-    NETLINK_CB(skb_out).dst_group = 0; // not in mcast group
-
-    // send the message
-    res = nlmsg_unicast(nl_sk, skb_out, pid);
-
-    if (res < 0)
-        printk(KERN_INFO NETLINK_LOG "error while sending back to user\n");
-}
-
-struct sock *nl_sk = NULL;
-
-static void freezer_recv_msg(struct sk_buff *skb)
-{
-    struct nlmsghdr *nlh;
-    struct sk_buff *skb_out;
-    int pid = 0;
-    int res = 0;
-    int freezer_wrapper_res = 0; 
-
-    printk(KERN_INFO NETLINK_LOG "entering: %s\n", __FUNCTION__);
-
-    nlh = (struct nlmsghdr *)skb->data;
-    struct netlink_cmd *data = (struct netlink_cmd *) nlmsg_data(nlh);
+    nlh = (struct nlmsghdr*)skb->data;
+    struct netlink_cmd* data = (struct netlink_cmd*)nlmsg_data(nlh);
     printk(KERN_INFO NETLINK_LOG "resource: %d\n", data->resource);
     printk(KERN_INFO NETLINK_LOG "uid: %d\n", data->uid);
     pid = nlh->nlmsg_pid; /*pid of sending process */
@@ -94,10 +53,7 @@ static void freezer_recv_msg(struct sk_buff *skb)
 static int __init freezer_init(void)
 {
     printk(KERN_INFO NETLINK_LOG "entering: %s\n", __FUNCTION__);
-    struct netlink_kernel_cfg cfg =
-    {
-        .input = freezer_recv_msg
-    };
+    struct netlink_kernel_cfg cfg = { .input = freezer_recv_msg };
 
     nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
     if (!nl_sk)
