3a10f1d6b654f7270b567ac7520cb9f424347cd1
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-29 00:52:06 +0200

    Update carto/src/users.c

--- carto/src/users.c
+++ carto/src/users.c
@@ -9,15 +9,10 @@ utmp_t* clone_utmp(utmp_t* utmp)
 {
     utmp_t* cloned_utmp = xmalloc(sizeof(utmp_t));
 
-    cloned_utmp->ut_type = utmp->ut_type;
-    cloned_utmp->ut_pid = utmp->ut_pid;
-    memcpy(cloned_utmp->ut_line, utmp->ut_line, __UT_LINESIZE);
-    memcpy(cloned_utmp->ut_id, utmp->ut_id, 4);
-    memcpy(cloned_utmp->ut_user, utmp->ut_user, __UT_NAMESIZE);
-    memcpy(cloned_utmp->ut_host, utmp->ut_host, __UT_HOSTSIZE);
-    cloned_utmp->ut_exit = utmp->ut_exit;
-    cloned_utmp->ut_session = utmp->ut_session;
-    cloned_utmp->ut_tv = utmp->ut_tv;
+    memcpy(cloned_utmp->ut_line, utmp->ut_line, UT_LINESIZE);
+    memcpy(cloned_utmp->ut_name, utmp->ut_name, UT_LINESIZE);
+    memcpy(cloned_utmp->ut_host, utmp->ut_host, UT_HOSTSIZE);
+    memcpy(cloned_utmp->ut_time, utmp->ut_time, sizeof(time_t));
 
     return cloned_utmp;
 }
