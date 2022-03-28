7e9221fefc550791b3404e8a7a2a880dd0341ba1
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-29 01:02:42 +0200

    Update carto/src/processes.c

--- carto/src/processes.c
+++ carto/src/processes.c
@@ -1,7 +1,7 @@
 #include "processes.h"
 
 #include <errno.h>
-#include <error.h>
+#include <err.h>
 #include <limits.h>
 #include <stdio.h>
 
@@ -12,7 +12,7 @@ char* proc_readlink(pid_t pid, const char* attribute)
     char buffer[PATH_MAX];
 
     if (snprintf(buffer, PATH_MAX, "/proc/%d/%s", pid, attribute) == -1)
-        error(1, errno, "sprintf error");
+        errc(1, errno, "sprintf error");
 
     return readlink_str(buffer);
 }
