56a734f4ed146d78d3136861e5cd874c3c011174
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-29 18:33:36 +0200

    Update carto/src/users.c

--- carto/src/users.c
+++ carto/src/users.c
@@ -7,14 +7,12 @@
 
 utmp_t* clone_utmp(utmp_t* utmp)
 {
-    puts("Before");
     utmp_t* cloned_utmp = xmalloc(sizeof(utmp_t));
 
     memcpy(cloned_utmp->ut_line, utmp->ut_line, UT_LINESIZE);
     memcpy(cloned_utmp->ut_name, utmp->ut_name, UT_LINESIZE);
     memcpy(cloned_utmp->ut_host, utmp->ut_host, UT_HOSTSIZE);
     cloned_utmp->ut_time = utmp->ut_time;
-    puts("After");
     return cloned_utmp;
 }
 
