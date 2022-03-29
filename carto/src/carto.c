0eb7b84b7807ef5a8b3ec63aac3a20db0317710f
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-29 18:20:21 +0200

    Update carto/src/carto.c

--- carto/src/carto.c
+++ carto/src/carto.c
@@ -36,13 +36,13 @@ utmp_t** get_users(void)
 
     Array* a = array_new();
 
-    puts("1");
+    puts("Before");
     while (fread((char*)&utmp, sizeof(utmp), 1, stdin) == 1)
     {
         utmp_t* cloned_utmp = clone_utmp(&utmp);
         array_push(a, cloned_utmp);
     }
-    puts("2");
+    puts("After");
 
     array_push(a, NULL);
 
