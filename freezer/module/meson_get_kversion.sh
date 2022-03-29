8a4ebfb377946a69c20dd768625a5a3e4a6fa81c
Michel San <michel1.san@epita.fr>
2022-03-29 11:13:36 +0200

    freezer: add script for meson

---
+++ freezer/module/meson_get_kversion.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Script used by meson.build to define the kernel version
+
+# if the script is ran from a gitlab pipeline -> get the version from env var
+# if ran locally, get the kernel version from `uname -r`
+
+if [ -n $(echo $IS_GITLAB_JOB) ]
+then
+    K_V=$(echo $KERNEL_VERSION)
+else
+    K_V=$(uname -r)
+fi
+
+echo $K_V
