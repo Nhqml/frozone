329c28914761ed00795493affb97bd20684bc890
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-30 23:25:53 +0200

    Update carto/src/carto.c, carto/src/processes.h

--- carto/src/carto.c
+++ carto/src/carto.c
@@ -6,29 +6,11 @@
 #include <time.h>
 #include <unistd.h>
 
-#include <sys/param.h>
-#include <sys/types.h>
-#include <sys/signal.h>
-#include <sys/sysctl.h>
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <sys/proc.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <ctype.h>
-#include <err.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <kvm.h>
-#include <locale.h>
-#include <nlist.h>
-#include <paths.h>
-#include <pwd.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <kvm.h>
 #include <limits.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
 
 #include "processes.h"
 #include "users.h"
@@ -64,112 +46,18 @@ utmp_t** get_users(void)
     return (utmp_t**)array_as_raw(a);
 }
 
-struct varent *vhead;
-int eval;
-kvm_t *kd;
-int kvm_sysctl_only;
-
 process_t** get_processes(void)
 {
-    struct varent *vent;
-    struct kinfo_proc *kp, **kinfo;
-    int what, flag, nentries, lineno, i;
     char errbuf[_POSIX2_LINE_MAX];
-    kd = kvm_open(NULL, NULL, NULL, KVM_NO_FILES, errbuf);
-    kvm_sysctl_only = 1;
-	if (kd == NULL)
-    {
-		errx(1, "%s", errbuf);
+    kvm_t *kernel = kvm_openfiles(NULL, NULL, NULL, KVM_NO_FILES, errbuf);
+    int nentries = 0;
+    struct kinfo_proc *kinfo = kvm_getprocs(kernel, KERN_PROC_ALL, 0, sizeof(struct kinfo_proc)
+    , &nentries);
+    int i;
+    for (i = 0; i < nentries; ++i) {
+        printf("%s\n", kinfo[i].p_comm);
     }
-    what = KERN_PROC_ALL;
-    flag = 0;
-    kp = kvm_getprocs(kd, what, flag, sizeof(*kp), &nentries);
-	if (kp == NULL)
-    {
-		errx(1, "%s", kvm_geterr(kd));
-    }
-    //printheader();
-	if (nentries == 0)
-    {
-		exit(1);
-    }
-	if ((kinfo = reallocarray(NULL, nentries, sizeof(*kinfo))) == NULL)
-    {
-		err(1, "failed to allocate memory for proc pointers");
-    }
-	for (i = 0; i < nentries; i++)
-    {
-		kinfo[i] = &kp[i];
-    }
-	for (i = lineno = 0; i < nentries; i++)
-    {
-		if (((int)kinfo[i]->p_tdev == NODEV || (kinfo[i]->p_psflags & PS_CONTROLT ) == 0))
-        {
-			continue;
-        }
-		if (kinfo[i]->p_tid == -1)
-        {
-			continue;
-        }
-		for (vent = vhead; vent; vent = vent->next)
-        {
-			(vent->var->oproc)(kinfo[i], vent);
-			if (vent->next != NULL)
-				(void)putchar(' ');
-		}
-		(void)putchar('\n');
-	}
-	exit(eval);
-
-    Array* pids = get_num_dir_contents("/proc");
-
-    Array* processes = array_with_capacity(pids->size + 1);
-
-    for (size_t i = 0; i < pids->size; ++i)
-    {
-        process_t* process = xcalloc(1, sizeof(process_t));
-        process->pid = *(int*)pids->array[i];
-
-        char path_buf[PATH_MAX];
-        if (snprintf(path_buf, PATH_MAX, "/proc/%d", process->pid) == -1)
-            errc(1, errno, "snprintf error");
-
-        struct stat process_stat;
-        if (stat(path_buf, &process_stat) == 0)
-        {
-            process->uid = process_stat.st_uid;
-            process->gid = process_stat.st_gid;
-            process->etime = time(NULL) - process_stat.st_ctime;
-        }
-
-        process->exe_path = proc_readlink(process->pid, "exe");
-        process->cwd = proc_readlink(process->pid, "cwd");
-        process->root = proc_readlink(process->pid, "root");
-
-        if (snprintf(path_buf, PATH_MAX, "/proc/%d/cmdline", process->pid) == -1)
-            errc(1, errno, "snprintf error");
-        FILE* f = fopen(path_buf, "r");
-        if (f != NULL)
-        {
-            char proc_cmdline[ARG_MAX];
-            size_t ret = fread(proc_cmdline, 1, ARG_MAX, f);
-            if (ret > 0)
-            {
-                process->cmdline = xmalloc(ret + 1);
-                memcpy(process->cmdline, proc_cmdline, ret);
-                process->cmdline[ret] = '\0';
-            }
-            fclose(f);
-        }
-
-        array_push(processes, process);
-    }
-
-    array_push(processes, NULL);
-
-    array_destroy(pids);
-
-    return (process_t**)array_as_raw(processes);
+    return 0;
 }
 
 void get_connections(void) {}
