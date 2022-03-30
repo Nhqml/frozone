e08febfd797e75e8bf8c7d57f8c121aba7146ea8
Michel San <michel1.san@epita.fr>
2022-03-30 19:19:30 +0200

    vagrant: add dependencies and packages

--- vagrant/vagrant_init_ubuntu.sh
+++ vagrant/vagrant_init_ubuntu.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 apt update
-apt install -y linux-headers-$(uname -r) build-essential
+apt install -y linux-headers-$(uname -r) build-essential python3 python3-pip pkg-config libcunit1 libcunit1-doc libcunit1-dev
+
+pip3 install meson && pip3 install ninja
