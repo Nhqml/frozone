92aec7d5a95f9696781fe6a907d1c97925e337d5
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-30 22:10:17 +0200

    Update carto/src/carto.c

--- carto/src/carto.c
+++ carto/src/carto.c
@@ -6,13 +6,29 @@
 #include <time.h>
 #include <unistd.h>
 
-#include <signal.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/signal.h>
+#include <sys/sysctl.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/proc.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <kvm.h>
-#include <sys/types.h>
-#include <sys/proc.h>
-#include <sys/sysctl.h>
-#include <sys/param.h>
+#include <locale.h>
+#include <nlist.h>
+#include <paths.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 #include "processes.h"
 #include "users.h"
