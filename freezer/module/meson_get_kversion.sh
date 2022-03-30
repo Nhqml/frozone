d7c8be3c05e7ddde9729857b4a5753d8e311c178
Michel San <michel1.san@epita.fr>
2022-03-30 19:19:10 +0200

    build: fix meson script to get kernel version

--- freezer/module/meson_get_kversion.sh
+++ freezer/module/meson_get_kversion.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script used by meson.build to define the kernel version
 
 # if the script is ran from a gitlab pipeline -> get the version from env var
 # if ran locally, get the kernel version from `uname -r`
 
-if [ -n $(echo $IS_GITLAB_JOB) ]
+if [ -n "$(echo $IS_GITLAB_JOB)" ]
 then
     K_V=$(echo $KERNEL_VERSION)
 else
