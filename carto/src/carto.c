72d5dc984a338a8efc1758ac07c0c1b6549d96b1
Erfan Hormozizadeh <erfan.hormozizadeh@epita.fr>
2022-03-31 19:45:57 +0200

    Update carto/src/carto.c

--- carto/src/carto.c
+++ carto/src/carto.c
@@ -89,7 +89,9 @@ process_t** get_processes(void)
     char errbuf[_POSIX2_LINE_MAX];
     kvm_t *kernel = kvm_openfiles(NULL, NULL, NULL, KVM_NO_FILES, errbuf);
     int nentries = 0;
+    puts("1");
     struct kinfo_proc *kinfo = kvm_getprocs(kernel, KERN_PROC_ALL, 0, sizeof(struct kinfo_proc), &nentries);
+    puts("2");
     Array* processes = array_with_capacity(nentries + 1);
     int i;
     for (i = 0; i < nentries; ++i) {
